849ffc846a250c99932f2f9fe19691a9a0e7559c
Philip Buzzanca <philbuzzanca@gmail.com>
2023-07-09 14:47:21 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as HashRouter, Router, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Resume from './pages/Resume'
@@ -7,14 +7,14 @@ import Portfolio from './pages/Portfolio'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <MenuBar />
       <Routes>
         <Route exact path='/' element={<Home />}/>
         <Route exact path='/resume' element={<Resume />}/>
         <Route exact path='/portfolio' element={<Portfolio />}/>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
